cc7753fb41ff6e47b9c0b578e4cc812b9e264808
david cardenas <101474305+Davidc5922@users.noreply.github.com>
2022-09-30 01:19:03 -0500

    Update getAllDogs.js

--- src/info/getAllDogs.js
+++ src/info/getAllDogs.js
@@ -3,11 +3,7 @@ const {getdbinfo} = require('./getdbinfo')
 
 const getAllDogs = async () => {
     const apiInfo = await getapiinfo()
-    const dbInfo = await getdbinfo()
-    const totalInfo = apiInfo.concat(dbInfo).sort( (a,b) =>{
-        return a.name < b.name ? -1 : 1
-    })
-    return totalInfo
+  return apiInfo
 }
 
 module.exports={getAllDogs}
